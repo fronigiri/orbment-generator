218b983b22a67435809f8f45cbd2d6b580f88af2
fronigiri <addihor97@gmail.com>
2025-08-05 21:51:42 -0400

    Added tests for skill_qa

--- test_skills.py
+++ test_skills.py
@@ -10,6 +10,15 @@ class TestSkill(unittest.TestCase):
         self.assertEqual(stone_hammer.earth, 1)
         self.assertEqual(stone_hammer.description,"Drops a large boulder on enemies." )
 
+    def test_skill_eq(self):
+        skill_1 = Skill(name="Stone Hammer", earth=1, description="Drops a large boulder on enemies.")
+        skill_2 = Skill(name="Stone Hammer", earth=1, description="Drops a large boulder on enemies.")
+        skill_3 = Skill(name="Earth Wall", earth=4, description="Forms a temporary earth shield perimeter. [Immunity]")
+        self.assertEqual(skill_1, skill_2)
+        self.assertEqual(skill_1, skill_2)
+        self.assertNotEqual(skill_3, skill_1)
+    
+    
 
 if __name__ == "__main__":
     unittest.main()
